c545d67117ac8bb149df36676db4fd72f48629f5
Uriah <codeitlikemiley@gmail.com>
2025-02-11 12:54:11 +0800

    better error handling

--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,4 @@
-use poem::{http::StatusCode, Error, Result};
+use poem::http::StatusCode;
 use serde::{Deserialize, Serialize};
 use serde_json::{from_str, to_string_pretty};
 use std::sync::Arc;
@@ -19,19 +19,13 @@ pub struct Database {
 
 const DB_FILE: &str = "data.json";
 
-pub async fn load_db() -> Result<Db> {
+pub async fn load_db() -> Result<Db, std::io::Error> {
     let contents = match read_to_string(DB_FILE).await {
         Ok(data) => data,
         Err(_) => {
-            // If the file does not exist, initialize an empty database
             let db = Database::default();
-            let db_json = to_string_pretty(&db).unwrap(); // Safe because we control the struct
-            write(DB_FILE, db_json).await.map_err(|_| {
-                Error::from_string(
-                    format!("Failed to create database file: {}", DB_FILE),
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                )
-            })?;
+            let db_json = to_string_pretty(&db).unwrap();
+            write(DB_FILE, db_json).await?;
             return Ok(Arc::new(Mutex::new(db)));
         }
     };
@@ -39,15 +33,9 @@ pub async fn load_db() -> Result<Db> {
     let db = match from_str::<Database>(&contents) {
         Ok(parsed) => parsed,
         Err(_) => {
-            // If parsing fails, initialize a new empty database
             let db = Database::default();
             let db_json = to_string_pretty(&db).unwrap();
-            write(DB_FILE, db_json).await.map_err(|_| {
-                Error::from_string(
-                    format!("Failed to reset database file: {}", DB_FILE),
-                    StatusCode::INTERNAL_SERVER_ERROR,
-                )
-            })?;
+            write(DB_FILE, db_json).await?;
             db
         }
     };
@@ -55,15 +43,15 @@ pub async fn load_db() -> Result<Db> {
     Ok(Arc::new(Mutex::new(db)))
 }
 
-pub async fn save_db(db_guard: &Database) -> Result<()> {
+pub async fn save_db(db_guard: &Database) -> poem::Result<()> {
     let json = to_string_pretty(db_guard).map_err(|_| {
         let message = format!("Failed to Read File from {}", DB_FILE);
-        Error::from_string(message, StatusCode::INTERNAL_SERVER_ERROR)
+        poem::Error::from_string(message, StatusCode::INTERNAL_SERVER_ERROR)
     })?;
 
     write(DB_FILE, json).await.map_err(|_| {
         let message = format!("Failed to write on {}", DB_FILE);
-        Error::from_string(message, StatusCode::INTERNAL_SERVER_ERROR)
+        poem::Error::from_string(message, StatusCode::INTERNAL_SERVER_ERROR)
     })?;
 
     Ok(())
