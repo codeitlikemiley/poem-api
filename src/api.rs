96742de1e4a8300ed93dae5a134f19dcf7c84f2b
Uriah <codeitlikemiley@gmail.com>
2025-02-11 18:25:23 +0800

    responses and return

--- src/api.rs
+++ src/api.rs
@@ -7,7 +7,7 @@ use crate::{
     db::{save_db, Db},
     models::Item,
     requests::{ItemRequest, LoginRequest},
-    responses::{Code, Response},
+    responses::{Responses, Return, Schema},
 };
 
 #[derive(Tags)]
@@ -25,25 +25,22 @@ impl Api {
         &self,
         server_key: Data<&ServerKey>,
         req: Json<LoginRequest>,
-    ) -> Response<String> {
+    ) -> Responses<String> {
         let user = AuthUser {
             username: req.0.username,
         }
         .sign_with_key(server_key.0);
 
         match user {
-            Ok(resource) => Response::Ok(Json(Code::success(resource))),
-            Err(_) => Response::InternalError(Json(Code::internal_error(
-                "Invalid Credentials".to_string(),
-            ))),
+            Ok(resource) => Return::ok(resource),
+            Err(e) => Return::internal_error(e.to_string()),
         }
     }
 
     #[oai(path = "/items", method = "get", tag = "Group::Items")]
-    async fn get_items(&self, db: Data<&Db>) -> Response<Vec<Item>> {
+    async fn get_items(&self, db: Data<&Db>) -> Responses<Vec<Item>> {
         let db = db.lock().await;
-
-        Response::Ok(Json(Code::success(db.items.clone())))
+        Return::success(db.items.clone())
     }
 
     #[oai(path = "/items", method = "post", tag = "Group::Items")]
@@ -52,7 +49,7 @@ impl Api {
         _auth: Authenticate,
         db: Data<&Db>,
         req: Json<ItemRequest>,
-    ) -> Response<Item> {
+    ) -> Responses<Item> {
         let mut db = db.lock().await;
 
         let new_item = Item {
@@ -64,18 +61,18 @@ impl Api {
         db.items.push(new_item.clone());
 
         match save_db(&db).await {
-            Ok(_) => Response::Created(Json(Code::created(new_item))),
-            Err(e) => Response::InternalError(Json(Code::internal_error(e.to_string()))),
+            Ok(_) => Return::created(new_item),
+            Err(e) => Return::internal_error(e.to_string()),
         }
     }
 
     #[oai(path = "/items/:id", method = "get", tag = "Group::Items")]
-    async fn get_item(&self, db: Data<&Db>, id: Path<u32>) -> Response<Item> {
+    async fn get_item(&self, db: Data<&Db>, id: Path<u32>) -> Responses<Item> {
         let db = db.lock().await;
 
         match db.items.iter().find(|item| item.id == *id).cloned() {
-            Some(item) => Response::Ok(Json(Code::success(item))),
-            None => Response::NotFound(Json(Code::not_found())),
+            Some(item) => Return::success(item),
+            None => Return::not_found(),
         }
     }
 
@@ -86,7 +83,7 @@ impl Api {
         db: Data<&Db>,
         id: Path<u32>,
         req: Json<ItemRequest>,
-    ) -> Response<Item> {
+    ) -> Responses<Item> {
         let mut db = db.lock().await;
 
         match db.items.iter_mut().find(|item| item.id == *id) {
@@ -95,11 +92,11 @@ impl Api {
                 let updated_item = item.clone();
 
                 match save_db(&db).await {
-                    Ok(_) => Response::Ok(Json(Code::success(updated_item))),
-                    Err(e) => Response::InternalError(Json(Code::internal_error(e.to_string()))),
+                    Ok(_) => Return::success(updated_item),
+                    Err(e) => Return::internal_error(e.to_string()),
                 }
             }
-            None => Response::NotFound(Json(Code::not_found())),
+            None => Responses::NotFound(Json(Schema::not_found())),
         }
     }
 
@@ -109,7 +106,7 @@ impl Api {
         _auth: Authenticate,
         db: Data<&Db>,
         id: Path<u32>,
-    ) -> Response<String> {
+    ) -> Responses<String> {
         let mut db = db.lock().await;
 
         match db.items.iter().position(|item| item.id == *id) {
@@ -117,13 +114,11 @@ impl Api {
                 db.items.remove(pos);
 
                 match save_db(&db).await {
-                    Ok(_) => {
-                        Response::Ok(Json(Code::success("Item deleted successfully".to_string())))
-                    }
-                    Err(e) => Response::InternalError(Json(Code::internal_error(e.to_string()))),
+                    Ok(_) => Return::ok("Item deleted successfully".to_string()),
+                    Err(e) => Return::internal_error(e.to_string()),
                 }
             }
-            None => Response::NotFound(Json(Code::not_found())),
+            None => Return::not_found(),
         }
     }
 }
--- src/responses.rs
+++ src/responses.rs
@@ -7,14 +7,25 @@ use poem_openapi::{
 };
 use serde::{Deserialize, Serialize};
 
+pub trait ApiData: ParseFromJSON + ToJSON + Send + Sync {}
+impl<T: ParseFromJSON + ToJSON + Send + Sync> ApiData for T {}
+
 #[derive(Object, Serialize, Deserialize)]
-pub struct Code<T: ParseFromJSON + ToJSON + Send + Sync> {
+pub struct Schema<T: ApiData> {
     code: u16,
     message: String,
     data: Option<T>,
 }
 
-impl<T: ParseFromJSON + ToJSON + Send + Sync> Code<T> {
+impl<T: ApiData> Schema<T> {
+    pub fn ok(msg: String) -> Self {
+        Self {
+            code: 200,
+            message: msg,
+            data: None,
+        }
+    }
+
     pub fn success(data: T) -> Self {
         Self {
             code: 200,
@@ -28,6 +39,14 @@ impl<T: ParseFromJSON + ToJSON + Send + Sync> Code<T> {
             code: 201,
             message: "Created".to_string(),
             data: Some(data),
+        }
+    }
+
+    pub fn deleted() -> Self {
+        Self {
+            code: 204,
+            message: "".to_string(),
+            data: None,
         }
     }
 
@@ -58,23 +77,65 @@ impl<T: ParseFromJSON + ToJSON + Send + Sync> Code<T> {
 
 #[derive(ApiResponse)]
 #[oai(bad_request_handler = "handle_bad_request")]
-pub enum Response<T: ParseFromJSON + ToJSON + Send + Sync> {
+pub enum Responses<T: ApiData> {
     #[oai(status = 200)]
-    Ok(Json<Code<T>>),
+    Ok(Json<Schema<T>>),
     #[oai(status = 201)]
-    Created(Json<Code<T>>),
+    Created(Json<Schema<T>>),
+    #[oai(status = 204)]
+    Deleted(Json<Schema<T>>),
     #[oai(status = 400)]
-    BadRequest(Json<Code<T>>),
+    BadRequest(Json<Schema<T>>),
     #[oai(status = 404)]
-    NotFound(Json<Code<T>>),
+    NotFound(Json<Schema<T>>),
     #[oai(status = 500)]
-    InternalError(Json<Code<T>>),
+    InternalError(Json<Schema<T>>),
 }
 
-fn handle_bad_request<T: ParseFromJSON + ToJSON + Send + Sync>(err: Error) -> Response<T> {
+fn handle_bad_request<T: ParseFromJSON + ToJSON + Send + Sync>(err: Error) -> Responses<T> {
     if err.is::<ParseRequestPayloadError>() {
-        Response::BadRequest(Json(Code::bad_request(err.to_string())))
+        Responses::BadRequest(Json(Schema::bad_request(err.to_string())))
     } else {
-        Response::InternalError(Json(Code::internal_error(err.to_string())))
+        Responses::InternalError(Json(Schema::internal_error(err.to_string())))
+    }
+}
+
+pub struct Return<T: ParseFromJSON + ToJSON + Send + Sync>(Responses<T>);
+
+impl<T: ParseFromJSON + ToJSON + Send + Sync> Return<T> {
+    pub fn success(data: T) -> Responses<T> {
+        Responses::Ok(Json(Schema::success(data)))
+    }
+
+    pub fn ok(message: impl Into<String>) -> Responses<T> {
+        Responses::Ok(Json(Schema::ok(message.into())))
+    }
+
+    pub fn deleted() -> Responses<T> {
+        Responses::Deleted(Json(Schema::deleted()))
+    }
+
+    pub fn created(data: T) -> Responses<T> {
+        Responses::Created(Json(Schema::created(data)))
+    }
+
+    pub fn not_found() -> Responses<T> {
+        Responses::NotFound(Json(Schema::not_found()))
+    }
+
+    pub fn bad_request(message: impl Into<String>) -> Responses<T> {
+        Responses::BadRequest(Json(Schema::bad_request(message.into())))
+    }
+
+    pub fn internal_error(message: impl Into<String>) -> Responses<T> {
+        Responses::InternalError(Json(Schema::internal_error(message.into())))
+    }
+
+    pub fn from_error(err: Error) -> Responses<T> {
+        if err.is::<ParseRequestPayloadError>() {
+            Self::bad_request(err.to_string())
+        } else {
+            Self::internal_error(err.to_string())
+        }
     }
 }
