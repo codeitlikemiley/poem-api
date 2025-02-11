07e0c18f83fbcac95cf01c275eb4572deddf67b2
Uriah <codeitlikemiley@gmail.com>
2025-02-11 14:44:06 +0800

    delete unneeded stuff

--- src/models.rs
+++ src/models.rs
@@ -1,15 +1,8 @@
-use poem_openapi::{Enum, Object, Tags};
+use poem_openapi::Object;
 use serde::{Deserialize, Serialize};
 
 #[derive(Object, Deserialize, Clone, Serialize)]
 pub struct Item {
     pub id: u32,
     pub name: String,
-}
-
-#[derive(Enum)]
-enum PetStatus {
-    Available,
-    Pending,
-    Sold,
 }
